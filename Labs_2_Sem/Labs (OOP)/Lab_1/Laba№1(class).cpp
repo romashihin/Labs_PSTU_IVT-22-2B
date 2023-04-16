d5e1c0ab72253f3a48c16296ed72c1d336b5e208
romashihin <126802060+romashihin@users.noreply.github.com>
2023-04-16 22:48:10 +0500

    Add files via upload

---
+++ Labs_2_Sem/Labs (OOP)/Lab_1/Laba№1(class).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Clock
+{
+	
+public:
+
+	int first; //h
+	int second;  //min
+
+	void Minutes()
+	{
+		cout << "Enter time in hours: ";
+		cin >> first;
+		second = first * 60;
+		cout << "Your time in minutes is: ";
+		cout << second << endl;
+	}
+};
+
+int main()
+{
+	Clock Chasi;
+	Chasi.Minutes();
+
+	return 0;
+
+}
