d07c1ec9e74b45e10dd4e47cb66d96dc19ccedde
romashihin <126802060+romashihin@users.noreply.github.com>
2023-04-16 23:35:51 +0500

    Add files via upload

---
+++ Labs_2_Sem/Exchange sorting/Exchange sorting.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+
+void swap(int& a, int& b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void BubbleSorting(int* mas, int count)
+{
+	for (int k = 0; k < count; k++)
+	{
+		for (int i = 0; i < count - 1 - k; i++)
+		{
+			if (mas[i] > mas[i + 1])
+			{
+				swap(mas[i], mas[i + 1]);
+			}
+		}
+	}
+};
+
+int main()
+{
+	srand(time(NULL));
+	const int count = 10;
+	int mas[count];
+	for (int i = 0; i < count; i++)
+	{
+		mas[i] = rand() % 33;
+		cout << mas[i] << " ";
+	}
+	cout << endl;
+	BubbleSorting(mas, count);
+	for (int i = 0; i < count; i++)
+	{
+		cout << mas[i] << " ";
+	}
+	return 0;
+}
