a84f6710e5c3dea4c6829e96e5c70f74375f3e6c
romashihin <126802060+romashihin@users.noreply.github.com>
2023-04-16 23:48:51 +0500

    Add files via upload

---
+++ Labs_2_Sem/Labs(Part 1)/Lab_3/Laba№3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double fact(int n)
+{
+	int factor = 1;
+	for (int i = 1; i < n + 1; i++)
+	{
+		factor *= i;
+	}
+	return factor;
+}
+
+double func(double x, int n)
+{
+	double res;
+	if (n == 1)
+	{
+		return 1;
+	}
+	res = ((n * n + 1 / fact(n)) * pow((x * 0.5), n));
+	return (res + func(x, n - 1));
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x;
+	int n;
+	cout << "Введите x и n" << endl;
+	cin >> x >> n;
+	cout << "Сумма последовательности равна " << func(x, n) << endl;
+}
