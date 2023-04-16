1d4fdddec955bab33aa7843e79148789377c6260
romashihin <126802060+romashihin@users.noreply.github.com>
2023-04-16 22:52:14 +0500

    Add files via upload

---
+++ Labs_2_Sem/Labs(Part 1)/Lab_5/Laba№5.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+    const int rows = 3;
+    const int cols = 3;
+    int arr[rows][cols];
+    unordered_map<int, int> freq;
+    int min_val = INT_MAX;
+    srand(time(NULL));
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = rand() % 41 - 20;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (freq.count(arr[i][j])) {
+                freq[arr[i][j]]++;
+            }
+            else {
+                freq[arr[i][j]] = 1;
+            }
+        }
+    }
+    for (auto& it : freq) {
+        if (it.second == 1 && it.first < min_val) {
+            min_val = it.first;
+        }
+    }
+    cout << "The minimum non-repeating element in the array is " << min_val << endl;
+    return 0;
+}
