21e70157b45afb6f720b1c19fbe1fc54af920e7b
romashihin <126802060+romashihin@users.noreply.github.com>
2023-04-16 23:20:49 +0500

    Add files via upload

---
+++ Labs_2_Sem/Sorting by counting/sorting by counting.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	int mask[3] = { 1, 2, 3 };
+	int arr[300];
+	for (int i = 0; i < 300; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+	cout << "Массив: " << endl;
+	for (int i = 0; i < 300; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << "Искомая маска:" << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << mask[i] << " ";
+	}
+	cout << endl;
+	bool res = false;
+	int stroka = 0;
+	for (int i = 0; i < 293; i++)
+	{
+		if (arr[i + 2] == mask[2] and arr[i + 1] == mask[1] and arr[i] == mask[0])
+		{
+			res = true;
+			stroka = i + 1;
+		}
+	}
+	if (res)
+	{
+		cout << "Искомая маска ";
+		for (int i = 0; i < 3; i++)
+		{
+			cout << mask[i] << " ";
+		}
+		cout << "найдена" << endl;
+		cout << "Маска начинается с элемента под номером " << stroka << endl;
+	}
+	else
+	{
+		cout << "Искомая маска не найдена" << endl;
+	}
+	return 0;
+}
