42f8c66ea19dd5877059e65f2651b08f0715de03
romashihin <126802060+romashihin@users.noreply.github.com>
2023-04-16 22:54:24 +0500

    Add files via upload

---
+++ Labs_2_Sem/Labs(Part 1)/Lab_6/Laba №6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	char stroka[256];
+	gets_s(stroka);
+	
+	for (int i = 0; i < strlen(stroka); i++)
+	{
+		if (stroka[i] == 'H' and stroka[i + 1] == 'e' and stroka[i + 2] == 'l' and stroka[i + 3] == 'l' and stroka[i + 4] == 'o')
+		{
+			a += 1;
+		}
+	}
+	for (int i = 0; i < a; i++)
+	{
+		cout << "Hello ";
+	}
+}
