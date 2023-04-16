55596d94bfd6e1022e2b6c9da4cb675992cd0e46
romashihin <126802060+romashihin@users.noreply.github.com>
2023-04-16 23:20:05 +0500

    Add files via upload

---
+++ Labs_2_Sem/Rate selection method/Metod stavki i vibora.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <random>
+#include <ctime>
+using namespace std;
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+int main() 
+{
+    srand(time(NULL));
+    int arr[100];
+    cout << "Start array: " << endl;
+    for (int i = 0; i < 100; i++)
+    {
+        arr[i] = rand() % 41 - 20; 
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    cout << "Sorted array:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
