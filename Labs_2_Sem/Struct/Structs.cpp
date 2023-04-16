3767845174292e342a62b54a86f2bdaf0c384fd5
romashihin <126802060+romashihin@users.noreply.github.com>
2023-04-16 23:21:16 +0500

    Add files via upload

---
+++ Labs_2_Sem/Struct/Structs.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+struct Advertising
+{
+	int num_of_adds;
+	int num_of_percent;
+	double earnings_for_one_add;
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Advertising Income;
+	cout << "Введите количество объявлений, показанных посетителям: ";
+	cin >> Income.num_of_adds;
+	cout << endl;
+
+	cout << "Введите количество процентов посетителей, которые нажали на объявление: ";
+	cin >> Income.num_of_percent;
+	double interest_rate = Income.num_of_percent * 0.01;
+	cout << "Процент, записанный в форме десятичной дроби: " << interest_rate << endl << endl;
+
+	cout << "Введите средний заработок за каждое нажатие объявление: ";
+	cin >> Income.earnings_for_one_add;
+	cout << endl;
+
+	cout << "Доход от показа рекламы на веб-сайте: " << Income.num_of_adds << " * " << interest_rate << " * " << Income.earnings_for_one_add << " = " << Income.num_of_adds * interest_rate * Income.earnings_for_one_add << endl;
+	return 0;
+}
